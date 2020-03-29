27793f1f53c74a2cbb7c17c88c370a640785f015
waquar <waquar.alam92@gmail.com>
2020-03-29 21:03:35 +0530

    initial build

---
+++ src/main/java/jenkins/selenium_testng_jenkins/App.java
@@ -0,0 +1,13 @@
+package jenkins.selenium_testng_jenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/jenkins/selenium_testng_jenkins/AppTest.java
@@ -0,0 +1,20 @@
+package jenkins.selenium_testng_jenkins;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
